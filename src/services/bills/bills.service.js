072e77c753751537d368e4653b7b79fefe1aa505
Marcos Almeida <marcoshenriqueb@gmail.com>
2017-10-25 21:17:17 -0200

    Add bills and menu services

---
+++ src/services/bills/bills.service.js
@@ -0,0 +1,29 @@
+// Initializes the `bills` service on path `/bills`
+const createService = require('feathers-mongoose');
+const createModel = require('../../models/bills.model');
+const hooks = require('./bills.hooks');
+const filters = require('./bills.filters');
+
+module.exports = function () {
+  const app = this;
+  const Model = createModel(app);
+  const paginate = app.get('paginate');
+
+  const options = {
+    name: 'bills',
+    Model,
+    paginate
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/bills', createService(options));
+
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('bills');
+
+  service.hooks(hooks);
+
+  if (service.filter) {
+    service.filter(filters);
+  }
+};
