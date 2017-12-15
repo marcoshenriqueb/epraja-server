b7e8ca6a8b9a5fc9a74d801ba17013ea93f6c72b
Marcos Almeida <marcoshenriqueb@gmail.com>
2017-12-15 11:26:56 -0200

    Fix unique together hook

--- src/hooks/unique-together.js
+++ src/hooks/unique-together.js
@@ -11,14 +11,12 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     const query = {};
     query[options.field] = hook.data[options.field];
     query[options.together] = hook.data[options.together];
-    hook.app.service(options.service).find({ query }).then((bills) => {
+    return hook.app.service(options.service).find({ query }).then((bills) => {
       if (bills.total) {
         throw new errors.Unprocessable(
           `Field ${options.field} has to be unique within ${options.together}.`
         );
       }
     });
-
-    return Promise.resolve(hook);
   };
 };
