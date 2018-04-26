afe5c8bb5872435827713da89c753a6a7731e2df
Marcos Almeida <marcoshenriqueb@gmail.com>
2018-04-26 00:18:20 -0300

    Adds bill patch hook to dates

--- src/services/bills/bills.hooks.js
+++ src/services/bills/bills.hooks.js
@@ -1,3 +1,4 @@
+const commonHooks = require('feathers-hooks-common');
 const uniqueTogether = require('./../../hooks/unique-together');
 
 module.exports = {
@@ -7,7 +8,20 @@ module.exports = {
     get: [],
     create: [ uniqueTogether({ field: 'table', together: ['business', 'billStatus'], service: 'bills' }) ],
     update: [],
-    patch: [],
+    patch: [ (hook) => {
+      if (!Object.keys(hook.data).includes('menuItems.$.itemStatus')) return Promise.resolve(hook);
+      
+      return hook.app.service('menu-item-statuses').get(hook.data['menuItems.$.itemStatus'], {})
+      .then((data) => {
+        if (data.name === 'Encaminhado') {
+          hook.data = Object.assign({}, hook.data, { 'menuItems.$.forwardedAt': Date.now() });
+        } else if (data.name === 'Entregue') {
+          hook.data = Object.assign({}, hook.data, { 'menuItems.$.deliveredAt': Date.now() });
+        }
+
+        return hook;
+      });
+    } ],
     remove: []
   },
 
