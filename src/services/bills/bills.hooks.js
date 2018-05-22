db9e3be1be1c331890d4cc6b6a59786b075f516e
Marcos Almeida <marcoshenriqueb@gmail.com>
2018-05-22 14:28:49 -0300

    Adds tables restrictions

--- src/services/bills/bills.hooks.js
+++ src/services/bills/bills.hooks.js
@@ -1,11 +1,28 @@
-const uniqueTogether = require('./../../hooks/unique-together');
+const { iff } = require('feathers-hooks-common');
+const { BadRequest } = require('feathers-errors');
 
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [ uniqueTogether({ field: 'table', together: ['business', 'billStatus'], service: 'bills' }) ],
+    create: [ iff((hook) => {
+      return new Promise((resolve) => {
+        hook.app.service('bill-statuses').find({}).then((response) => {
+          hook.app.service('bills').find({
+            query: {
+              table: hook.data.table,
+              billStatus: {
+                $in: response.data.filter(s => s.name === 'Aberta' || s.name === 'Fechada')
+                  .map(s => s._id),
+              },
+            },
+          }).then((r) => {
+            resolve(!!r.data.length);
+          });
+        });
+      });
+    }, () => Promise.reject(new BadRequest('Table already in use.'))) ],
     update: [],
     patch: [ (hook) => {
       if (!Object.keys(hook.data).includes('menuItems.$.itemStatus')) return Promise.resolve(hook);
